ca83ff197f34603dc84bc4f9b66d50d9f8973a29
Ruskin Kollin <ruskin.kollin@gmail.com>
2021-04-12 16:01:52 +0300

    more tests added

--- Part 4/tests/blog_api.test.js
+++ Part 4/tests/blog_api.test.js
@@ -35,21 +35,86 @@ beforeEach(async () => {
 describe('GET /blogs', function () {
     test('blogs are returned as json', async () => {
         await api
-          .get('/api/blogs')
-          .expect(200)
-          .expect('Content-Type', /application\/json/);
-      });
-    
-      test('correct amount of blogs', async () => {
+            .get('/api/blogs')
+            .expect(200)
+            .expect('Content-Type', /application\/json/);
+    });
+
+    test('correct amount of blogs', async () => {
         const blogs = await api.get('/api/blogs');
         expect(blogs.body.length).toBe(2);
-      });
-    
-      test('verify unique identifier property of blog by id', async () => {
+    });
+
+    test('verify unique identifier property of blog by id', async () => {
         const blogs = await api.get('/api/blogs');
         expect(blogs.body[0].id).toBeDefined();
-      });
     });
+});
+
+describe('POST /blogs', function () {
+    test('create a new blog', async () => {
+        const newBlog = {
+            title: 'If Cats Disappeared From The World',
+            author: 'Genki Kawamura',
+            url: 'https://www.goodreads.com/book/show/40740914-if-cats-disappeared-from-the-world',
+            likes: 10,
+        };
+
+        await api
+            .post('/api/blogs')
+            .send(newBlog)
+            .expect(200)
+            .expect('Content-Type', /application\/json/)
+
+        const response = await api.get('/api/blogs')
+
+        expect(response.body).toHaveLength(initialBlogs.length + 1)
+        expect(response.body).toContainEqual(expect.objectContaining(newBlog))
+    })
+
+
+    test('blog without property likes, will have 0', async () => {
+        const newBlog = {
+            title: 'If Cats Disappeared From The World',
+            author: 'Genki Kawamura',
+            url: 'https://www.goodreads.com/book/show/40740914-if-cats-disappeared-from-the-world'
+        };
+      
+        const response = await api
+          .post('/api/blogs')
+          .send(newBlog)
+          .expect(400)
+          .expect('Content-Type', /application\/json/)
+
+        expect(response.body.likes).toBe(0)
+      })
+    })
+
+    test('400 request code if title is missing', async () => {
+        const blogNoTitle = {
+          author: 'Genki Kawamura',
+          url: 'https://www.goodreads.com/book/show/40740914-if-cats-disappeared-from-the-world',
+          likes: 10
+        }
+    
+        await api
+          .post('/api/blogs')
+          .send(blogNoTitle)
+          .expect(400)
+      })
+    
+      test('400 request code if url is missing', async () => {
+        const blogNoURL = {
+          title: 'If Cats Disappeared From The World',
+          author: 'Genki Kawamura',
+          likes: 10
+        }
+    
+        await api
+          .post('/api/blogs')
+          .send(blogNoURL)
+          .expect(400)
+      })
 
     afterAll(() => {
         mongoose.connection.close()
