351199ab81435bb12dee75209aa828b115529bca
Ruskin Kollin <ruskin.kollin@gmail.com>
2021-04-16 00:08:11 +0300

    further tests done

--- Part 4/tests/blog_api.test.js
+++ Part 4/tests/blog_api.test.js
@@ -3,6 +3,7 @@ const supertest = require('supertest')
 const app = require('../app')
 const api = supertest(app)
 const Blog = require('../models/blog')
+const helper = require('../utils/test_helper')
 
 const initialBlogs = [
     {
@@ -32,7 +33,7 @@ beforeEach(async () => {
 })
 
 
-describe('GET /blogs', function () {
+describe('when there is initially some blogs saved', function () {
     test('blogs are returned as json', async () => {
         await api
             .get('/api/blogs')
@@ -40,19 +41,19 @@ describe('GET /blogs', function () {
             .expect('Content-Type', /application\/json/);
     });
 
-    test('correct amount of blogs', async () => {
+    test('shows correct amount of blogs', async () => {
         const blogs = await api.get('/api/blogs');
         expect(blogs.body.length).toBe(2);
     });
 
-    test('verify unique identifier property of blog by id', async () => {
+    test('verifies unique identifier property of blog by id', async () => {
         const blogs = await api.get('/api/blogs');
         expect(blogs.body[0].id).toBeDefined();
     });
 });
 
-describe('POST /blogs', function () {
-    test('create a new blog', async () => {
+describe('addition of a new blog', function () {
+    test('succeeds with valid data', async () => {
         const newBlog = {
             title: 'If Cats Disappeared From The World',
             author: 'Genki Kawamura',
@@ -73,48 +74,93 @@ describe('POST /blogs', function () {
     })
 
 
-    test('blog without property likes, will have 0', async () => {
+    test('returns 0 for blog created without the property: likes', async () => {
         const newBlog = {
             title: 'If Cats Disappeared From The World',
             author: 'Genki Kawamura',
             url: 'https://www.goodreads.com/book/show/40740914-if-cats-disappeared-from-the-world'
         };
-      
+
         const response = await api
-          .post('/api/blogs')
-          .send(newBlog)
-          .expect(400)
-          .expect('Content-Type', /application\/json/)
+            .post('/api/blogs')
+            .send(newBlog)
+            .expect(400)
+            .expect('Content-Type', /application\/json/)
 
         expect(response.body.likes).toBe(0)
-      })
     })
 
-    test('400 request code if title is missing', async () => {
+    test('fails with statuscode 404 if title does not exist', async () => {
         const blogNoTitle = {
-          author: 'Genki Kawamura',
-          url: 'https://www.goodreads.com/book/show/40740914-if-cats-disappeared-from-the-world',
-          likes: 10
+            author: 'Genki Kawamura',
+            url: 'https://www.goodreads.com/book/show/40740914-if-cats-disappeared-from-the-world',
+            likes: 10
         }
     
         await api
-          .post('/api/blogs')
-          .send(blogNoTitle)
-          .expect(400)
-      })
+            .post('/api/blogs')
+            .send(blogNoTitle)
+            .expect(400)
+    })
     
-      test('400 request code if url is missing', async () => {
+    test('fails with statuscode 404 if URL does not exist', async () => {
         const blogNoURL = {
-          title: 'If Cats Disappeared From The World',
-          author: 'Genki Kawamura',
-          likes: 10
+            title: 'If Cats Disappeared From The World',
+            author: 'Genki Kawamura',
+            likes: 10
         }
     
         await api
-          .post('/api/blogs')
-          .send(blogNoURL)
-          .expect(400)
-      })
+            .post('/api/blogs')
+            .send(blogNoURL)
+            .expect(400)
+    })
+})
+
+
+describe('deletion of a note', function () {
+    test('succeeds with status code 204 if ID is valid', async () => {
+        const blogsAtStart = await helper.blogsInDb()
+        const blogToDelete = blogsAtStart[0]
+
+        await api
+            .delete(`/api/blogs/${blogToDelete.id}`)
+            .expect(204);
+
+        const blogsAtEnd = await helper.blogsInDb();
+        expect(blogsAtStart).toHaveLength(blogsAtEnd.length + 1)
+    });
+});
+
+describe('updating a blog', function () {
+    test('update likes of blog post', async () => {
+        const newBlog = {
+            title: 'If Cats Disappeared From The World',
+            author: 'Genki Kawamura',
+            url: 'https://www.goodreads.com/book/show/40740914-if-cats-disappeared-from-the-world',
+            likes: 10,
+        };
+  
+      await api
+        .post('/api/blogs')
+        .send(newBlog);
+  
+      const blogsAtStart = await helper.blogsInDb();
+      const blogToUpdate = blogsAtStart[blogsAtStart.length - 1].id;
+  
+      const newLikes = {
+        likes: 200,
+      };
+
+      const updatedBlog = await api
+        .put(`/api/blogs/${blogToUpdate}`)
+        .send(newLikes)
+        .set('Accept', 'application/json')
+        .expect(200);
+  
+      expect(updatedBlog.body).toHaveProperty('likes', 200);
+    });
+});
 
     afterAll(() => {
         mongoose.connection.close()
