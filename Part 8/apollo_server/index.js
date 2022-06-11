80443652146c52fba1f529d56c9454d0d1eaef7f
rus <ruskin.kollin@gmail.com>
2022-06-11 15:52:02 +0300

    Part 8 started.

---
+++ Part 8/apollo_server/index.js
@@ -0,0 +1,208 @@
+const { ApolloServer, gql } = require('apollo-server')
+const { v1: uuid } = require('uuid')
+
+let authors = [
+  {
+    name: 'Robert Martin',
+    id: "afa51ab0-344d-11e9-a414-719c6709cf3e",
+    born: 1952,
+  },
+  {
+    name: 'Martin Fowler',
+    id: "afa5b6f0-344d-11e9-a414-719c6709cf3e",
+    born: 1963
+  },
+  {
+    name: 'Fyodor Dostoevsky',
+    id: "afa5b6f1-344d-11e9-a414-719c6709cf3e",
+    born: 1821
+  },
+  { 
+    name: 'Joshua Kerievsky', // birthyear not known
+    id: "afa5b6f2-344d-11e9-a414-719c6709cf3e",
+  },
+  { 
+    name: 'Sandi Metz', // birthyear not known
+    id: "afa5b6f3-344d-11e9-a414-719c6709cf3e",
+  },
+]
+
+/*
+ * Suomi:
+ * Saattaisi olla järkevämpää assosioida kirja ja sen tekijä tallettamalla kirjan yhteyteen tekijän nimen sijaan tekijän id
+ * Yksinkertaisuuden vuoksi tallennamme kuitenkin kirjan yhteyteen tekijän nimen
+ *
+ * English:
+ * It might make more sense to associate a book with its author by storing the author's id in the context of the book instead of the author's name
+ * However, for simplicity, we will store the author's name in connection with the book
+*/
+
+let books = [
+  {
+    title: 'Clean Code',
+    published: 2008,
+    author: 'Robert Martin',
+    id: "afa5b6f4-344d-11e9-a414-719c6709cf3e",
+    genres: ['refactoring']
+  },
+  {
+    title: 'Agile software development',
+    published: 2002,
+    author: 'Robert Martin',
+    id: "afa5b6f5-344d-11e9-a414-719c6709cf3e",
+    genres: ['agile', 'patterns', 'design']
+  },
+  {
+    title: 'Refactoring, edition 2',
+    published: 2018,
+    author: 'Martin Fowler',
+    id: "afa5de00-344d-11e9-a414-719c6709cf3e",
+    genres: ['refactoring']
+  },
+  {
+    title: 'Refactoring to patterns',
+    published: 2008,
+    author: 'Joshua Kerievsky',
+    id: "afa5de01-344d-11e9-a414-719c6709cf3e",
+    genres: ['refactoring', 'patterns']
+  },  
+  {
+    title: 'Practical Object-Oriented Design, An Agile Primer Using Ruby',
+    published: 2012,
+    author: 'Sandi Metz',
+    id: "afa5de02-344d-11e9-a414-719c6709cf3e",
+    genres: ['refactoring', 'design']
+  },
+  {
+    title: 'Crime and punishment',
+    published: 1866,
+    author: 'Fyodor Dostoevsky',
+    id: "afa5de03-344d-11e9-a414-719c6709cf3e",
+    genres: ['classic', 'crime']
+  },
+  {
+    title: 'The Demon ',
+    published: 1872,
+    author: 'Fyodor Dostoevsky',
+    id: "afa5de04-344d-11e9-a414-719c6709cf3e",
+    genres: ['classic', 'revolution']
+  },
+]
+
+const typeDefs = gql`
+  type Query {
+    bookCount: Int!
+    authorCount: Int!
+    allBooks(author: String, genre: String): [Book!]!
+    allAuthors: [AuthorObject]
+  }
+
+  type Book {
+    title: String!
+    published: Int!
+    author: String!
+    id: String!
+    genres: [String!]!
+  }
+
+  type Author {
+    name: String!
+    id: String!
+    born: Int
+  }
+
+  type AuthorObject {
+    name: String
+    born: Int
+    bookCount: Int
+  }
+
+  type AuthorBookObject {
+    name: String
+    title: Int
+  }
+
+  type Mutation {
+    addBook(
+      title: String!
+      author: String!
+      published: Int!
+      genres: [String!]!
+    ): Book
+    editAuthor(
+      name: String
+      setBornTo: Int
+    ): Author
+  }
+`
+
+const resolvers = {
+  Query: {
+      bookCount: () => books.length,
+      authorCount: () => authors.length,
+      allBooks:  (root, args) => {
+        if (args.author && !args.genre) {
+          const booklist = books.filter(book => book.author === args.author)
+          return booklist
+        } else  if (!args.author && args.genre) {
+            const booklist = books.filter(book => book.genres.includes(args.genre))
+            return booklist
+        } else if (args.author && args.genre) {
+            const booklist = books.filter(book => book.genres.includes(args.genre) && book.author === args.author )
+            return booklist
+        } else {
+            return books
+        }
+      },
+      allAuthors: () => {
+        const authorArray = []
+        authors.forEach(author => {
+            const authorBookCount = books.filter(book => book.author === author.name).length
+            const authorObject = {name : author.name, bookCount: authorBookCount}
+            authorArray.push(authorObject)
+          })
+
+        return authorArray
+      }
+
+      //8.3
+    //   allAuthors: () => {
+    //     const authorArray = []
+    //     authors.forEach(author => {
+    //         const authorBookCount = books.filter(book => book.author === author.name).length
+    //         const authorObject = {name : author.name, bookCount: authorBookCount}
+    //         authorArray.push(authorObject)
+    //       })
+
+    //     return authorArray
+    //   }
+  },
+
+  Mutation: {
+    addBook: (root, args) => {
+      const book = { ...args, id: uuid() }
+      books = books.concat(book)
+      console.log('book', book)
+      return book
+    },
+    editAuthor: (root, args) => {
+        console.log('args ', args)
+        const author = authors.find(p => p.name === args.name)
+        if (!author) {
+            return null
+        }
+        const newAuthor = {...author, born: args.setBornTo}
+        authors = authors.map(p => p.name === args.name ? newAuthor: p)
+        return newAuthor
+      }
+  }
+}
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+})
+
+server.listen().then(({ url }) => {
+  console.log(`Server ready at ${url}`)
+})
