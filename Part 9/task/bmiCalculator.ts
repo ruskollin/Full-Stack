db9dc191ffa1dbb0313cd8f0fdfb5d2a20e1a5f0
rus <ruskin.kollin@gmail.com>
2023-03-04 14:01:18 +0200

    Initial tasks and 9.1 done.

---
+++ Part 9/task/bmiCalculator.ts
@@ -0,0 +1,19 @@
+interface BMIValues {
+    value1: number;
+    value2: number;
+  }
+  
+  const calculateBmi = (a: number, b: number) => {
+    const bmi: number = b / ((a * a) / 10000)
+    console.log(bmi)
+
+    if (bmi < 18.6) {
+        console.log( `Underweight` )
+    } else if (bmi >= 18.6 && bmi < 24.9) {
+        console.log(`Normal (healthy weight)` )
+    } else {
+        console.log(`Overweight` )
+    }
+  }
+
+ calculateBmi(180, 174)
