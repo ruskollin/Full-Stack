77fb730f4d6b9150b599961667d7ffc8e0202350
rus <ruskin.kollin@gmail.com>
2023-03-05 16:44:09 +0200

    9.3 done

--- Part 9/task/bmiCalculator.ts
+++ Part 9/task/bmiCalculator.ts
@@ -1,7 +1,6 @@
-
+//9.3
   const calculateBmi = (a: number, b: number) => {
     const bmi: number = b / ((a * a) / 10000)
-    console.log(bmi)
 
     if (bmi < 18.6) {
         console.log( `Underweight` )
@@ -12,4 +11,23 @@
     }
   }
 
- calculateBmi(180, 174)
+  const a: number = Number(process.argv[2])
+  const b: number = Number(process.argv[3])
+
+  calculateBmi(a, b);
+
+//9.2
+//  const calculateBmi = (a: number, b: number) => {
+//   const bmi: number = b / ((a * a) / 10000)
+//   console.log(bmi)
+
+//   if (bmi < 18.6) {
+//       console.log( `Underweight` )
+//   } else if (bmi >= 18.6 && bmi < 24.9) {
+//       console.log(`Normal (healthy weight)` )
+//   } else {
+//       console.log(`Overweight` )
+//   }
+// }
+
+// calculateBmi(180, 174)
--- Part 9/task/exerciseCalculator.ts
+++ Part 9/task/exerciseCalculator.ts
@@ -1,65 +1,161 @@
+//9.3
 interface Result {
-    periodLength: number;
-    trainingDays: number;
-    isSuccess: boolean;
-    rating: number;
-    ratingText: string;
-    target: number;
-    averageTime: number;
+  periodLength: number;
+  trainingDays: number;
+  isSuccess: boolean;
+  rating: number;
+  ratingText: string;
+  target: number;
+  averageTime: number;
 }
 
-const calculateExercise = (hours: number[], target: number) => {
+const calculateExercise = (target: number, hours: Array<number>): Result => {
 
-    //the number of days
-    const periodLength = hours.length;
+  //the number of days
+  const periodLength = hours.length;
 
-    //the number of training days
-    const trainingDays = hours.filter((hour) => hour > 0).length;
+  //the number of training days
+  const trainingDays = hours.filter((hour) => hour > 0).length;
 
-    //the original target value = target
+  //the original target value = target
 
-    //the calculated average time
-    const averageTime = hours.reduce((a, b) => a + b, 0) / periodLength;
+  //the calculated average time
+  const averageTime = hours.reduce((a, b) => a + b, 0) / periodLength;
 
-    //boolean value describing if the target was reached
-    const isSuccess = averageTime >= target;
+  //boolean value describing if the target was reached
+  const isSuccess = averageTime >= target;
 
-    //a rating between the numbers 1-3 that tells how well the hours are met. You can decide on the metric on your own.
-    const handleRating = (average: number, target: number) => {
-        if (average >= target) {
-            return 3
-        } else if (average < target && average > (target / 2) ) {
-            return 2
-        } else if (average < target && average <= (target / 2) ) {
-            return 3
-        }
-    };
+  //a rating between the numbers 1-3 that tells how well the hours are met. You can decide on the metric on your own.
+  const handleRating = (average: number, target: number) => {
+    if (average >= target) {
+      return 3
+    } else if (average < target && average > (target / 2)) {
+      return 2
+    } else if (average < target && average <= (target / 2)) {
+      return 3
+    }
+  };
 
-    const rating = handleRating(averageTime, target);
+  const rating = handleRating(averageTime, target);
 
-    //a text value explaining the rating, you can come up with the explanations
-    const handleRatingText = (rating: number) => {
-        if (rating === 1) {
-          return "try harder!";
-        } else if (rating === 2) {
-          return "not too bad but could be better!";
-        } else if (rating === 1) {
-          return "you are a beast!";
-        }
-    };
+  //a text value explaining the rating, you can come up with the explanations
+  const handleRatingText = (rating: number) => {
+    if (rating === 1) {
+      return "try harder!";
+    } else if (rating === 2) {
+      return "not too bad but could be better!";
+    } else if (rating === 3) {
+      return "you are a beast!";
+    }
+  };
 
-    const ratingText = handleRatingText(rating);
+  const ratingText = handleRatingText(rating);
 
+  return {
+    periodLength,
+    trainingDays,
+    isSuccess,
+    rating,
+    ratingText,
+    target,
+    averageTime
+  };
+};
+
+interface CommandLineInput {
+  target: number;
+  hours: number[];
+}
+
+const parseArguments = (args: string[]): CommandLineInput => {
+  if (args.length < 4) throw new Error("Not enough arguments");
+
+  if (args.slice(2).every(num => !isNaN(Number(num)))) {
+    const target = Number(args[2]);
+    const hours = args.slice(3).map(hour => Number(hour));
     return {
-        periodLength,
-        trainingDays,
-        isSuccess,
-        rating,
-        ratingText,
-        target,
-        averageTime,
+      target,
+      hours
     };
+  } else {
+    throw new Error('Provided values were not numbers!');
+  }
+};
 
+try {
+  const {
+    target,
+    hours
+  } = parseArguments(process.argv);
+  console.log(calculateExercise(target, hours));
+} catch (error) {
+  console.log(error.message);
 }
 
-console.log(calculateExercise([3, 0, 2, 4.5, 0, 3, 1], 2))
+//9.2
+
+// interface Result {
+//     periodLength: number;
+//     trainingDays: number;
+//     isSuccess: boolean;
+//     rating: number;
+//     ratingText: string;
+//     target: number;
+//     averageTime: number;
+// }
+
+// const calculateExercise = (hours: number[], target: number) => {
+
+//     //the number of days
+//     const periodLength = hours.length;
+
+//     //the number of training days
+//     const trainingDays = hours.filter((hour) => hour > 0).length;
+
+//     //the original target value = target
+
+//     //the calculated average time
+//     const averageTime = hours.reduce((a, b) => a + b, 0) / periodLength;
+
+//     //boolean value describing if the target was reached
+//     const isSuccess = averageTime >= target;
+
+//     //a rating between the numbers 1-3 that tells how well the hours are met. You can decide on the metric on your own.
+//     const handleRating = (average: number, target: number) => {
+//         if (average >= target) {
+//             return 3
+//         } else if (average < target && average > (target / 2) ) {
+//             return 2
+//         } else if (average < target && average <= (target / 2) ) {
+//             return 3
+//         }
+//     };
+
+//     const rating = handleRating(averageTime, target);
+
+//     //a text value explaining the rating, you can come up with the explanations
+//     const handleRatingText = (rating: number) => {
+//         if (rating === 1) {
+//           return "try harder!";
+//         } else if (rating === 2) {
+//           return "not too bad but could be better!";
+//         } else if (rating === 3) {
+//           return "you are a beast!";
+//         }
+//     };
+
+//     const ratingText = handleRatingText(rating);
+
+//     return {
+//         periodLength,
+//         trainingDays,
+//         isSuccess,
+//         rating,
+//         ratingText,
+//         target,
+//         averageTime,
+//     };
+
+// }
+
+// console.log(calculateExercise([3, 0, 2, 4.5, 0, 3, 1], 2))
